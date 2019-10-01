2b9a9189d68aeafdde8c577982dec6b1b51162f0
ganada123 <dksodalove@gmail.com>
2019-10-01 15:44:46 +0900

    HelloWorldServer and Client 1st

---
+++ HelloWorldServer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#define PORT 10000
+
+char buffer[100] ="Hello World";
+
+int main()
+{
+	int c_socket, s_socket;
+	struct sockaddr_in s_addr,c_addr;
+	int len;
+	int n;
+	
+	//1. 서버 소켓 생성
+	//서버소켓 = 클라이언트의 접속 요청을 처리(허용)해 주기 위한 소켓
+	s_socket = socket(PF_INET, SOCK_STREAM,0); // TCP/IP 통신을 위한 서버 소켓 생성
+	
+	//2. 서버 소켓 주소 설정
+	memset(&s_addr, 0, sizeof(s_addr)); // s_addr의 값을 모두 0으로 초기화
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY); // IP주소 설정
+	s_addr.sin_family= AF_INET;
+	s_addr.sin_port = htons(PORT);
+
+	//3. 서버 소켓 바인딩 ((-이미 포트를 사용하고 있는 경우 바인딩 실패한다))
+	if(bind(s_socket,(struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) //바인딩 작업 실패시, Cannot Bind 메세지 출력 후 프로그램 종료
+	{
+		printf("Cannot Bind\n");
+		return -1;
+	}
+
+	//4.listen() 함수 실행
+	if(listen(s_socket,5) == -1 )
+	{
+		printf("listen Fail\n");
+		return -1;
+	}
+
+	//5.
+	while(1)
+	{
+		len=sizeof(c_addr);
+		printf("클라이언트 접속을 기다리는 중....\n");
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len); //클라이언트의 요청이 오면 허용(accept)해주고, 클라이언트와 통실할 수 있도록 클라이언트 소켓(c_socket)을 반환
+		printf("클라이언트 접속 허용\n");
+
+		n=strlen(buffer);
+		write(c_socket, buffer, n); //클라이언트에게 buffer의 내용을 전송
+		
+		close(c_socket);		
+	}
+} 
